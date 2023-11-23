962611871cd1e36104e388aae087c8a3e46d807d
cp <iamdchinh@gmail.com>
2023-11-23 22:16:50 +0700

    Add soda scan task

--- dags/tasks/soda_task.py
+++ dags/tasks/soda_task.py
@@ -1,37 +1,20 @@
-from airflow.decorators import task
+from airflow.operators.bash import BashOperator
+from conf import config
 
 
-@task.external_python(
-    python="/opt/airflow/soda_venv/bin/python", task_id="run_soda_scan"
-)
-def check_load_raw():
-    PROJECT_ROOT = "/opt/airflow"
+def soda_scan(
+    task_id: str,
+    data_source: str,
+    soda_configuration_path: str,
+    sodacl_yaml_file_path: str,
+):
+    activate_virtualenv_command = f"source {config.PROJECT_ROOT}/soda_venv/bin/activate"
+    soda_scan_command = f"soda scan --data-source {data_source} --configuration  {soda_configuration_path} {sodacl_yaml_file_path}"
+    deactivate_virtualenv_command = "deactivate"
 
-    def run_soda_scan(project_root, scan_name, checks_subpath=None):
-        from soda.scan import Scan
+    soda_scan = BashOperator(
+        task_id=task_id,
+        bash_command=f"{activate_virtualenv_command} && {soda_scan_command} && {deactivate_virtualenv_command}",
+    )
 
-        print("Running Soda Scan ...")
-        config_file = f"{project_root}/soda/configuration.yml"
-        checks_path = f"{project_root}/soda/checks"
-
-        if checks_subpath:
-            checks_path += f"/{checks_subpath}"
-
-        data_source = "nyc_trip"
-
-        scan = Scan()
-        scan.set_verbose()
-        scan.add_configuration_yaml_file(config_file)
-        scan.set_data_source_name(data_source)
-        scan.add_sodacl_yaml_files(checks_path)
-        scan.set_scan_definition_name(scan_name)
-
-        result = scan.execute()
-        print(scan.get_logs_text())
-
-        if result != 0:
-            raise ValueError("Soda Scan failed")
-
-        return result
-
-    run_soda_scan(PROJECT_ROOT, "check_ingest_raw", checks_subpath="raw_data_check.yml")
+    return soda_scan
